e45e91b589f2379600c501741d9e0218bf3c4ee2
Kurtis <kthrasher1@hotmail.co.uk>
2024-10-09 02:46:57 +0100

    added printables link

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const striptags = require("striptags");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -10,11 +12,23 @@ module.exports = function (eleventyConfig) {
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
-  // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
+  // Set up markdown-it with HTML support
+  const md = markdownIt({
+    html: true, // Allow HTML tags in markdown
+  }).use(markdownItAnchor);
 
-    if(dateObj === "null"){
-      return "Present"
+  // Set the markdown library
+  eleventyConfig.setLibrary("md", md);
+
+  // Set the markdown filter
+  eleventyConfig.addFilter("markdown", (content) => {
+    return md.render(content);
+  });
+
+  // Human readable date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    if (dateObj === "null") {
+      return "Present";
     }
 
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
@@ -22,19 +36,20 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  // Sort items by date
   eleventyConfig.addFilter('sortByDate', (items) => {
     return items.sort((a, b) => {
-        return DateTime.fromISO(b.data.published) - DateTime.fromISO(a.data.published);
+      return DateTime.fromISO(b.data.published) - DateTime.fromISO(a.data.published);
     });
   });
 
+  // Extract excerpt for articles
   eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
+  // Support .yaml Extension in _data
   eleventyConfig.addDataExtension("yaml", (contents) =>
     yaml.safeLoad(contents)
   );
@@ -52,8 +67,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/files");
   eleventyConfig.addPassthroughCopy("./src/static/js");
   eleventyConfig.addPassthroughCopy("./src/static/favicon");
+
   // Let Eleventy transform HTML files as nunjucks
-  // So that we can use .html instead of .njk
   return {
     dir: {
       input: "src",
@@ -65,7 +80,7 @@ module.exports = function (eleventyConfig) {
 function extractExcerpt(article) {
   if (!article.hasOwnProperty("templateContent")) {
     console.warn(
-        'Failed to extract excerpt: Document has no property "templateContent".'
+      'Failed to extract excerpt: Document has no property "templateContent".'
     );
     return null;
   }
@@ -74,8 +89,8 @@ function extractExcerpt(article) {
   const content = article.templateContent;
 
   excerpt = striptags(content)
-      .substring(0, 1000) // Cap at 200 characters
-      .replace(/^\\s+|\\s+$|\\s+(?=\\s)/g, "")
-      .trim();
+    .substring(0, 1000) // Cap at 1000 characters
+    .replace(/^\\s+|\\s+$|\\s+(?=\\s)/g, "")
+    .trim();
   return excerpt;
 }
